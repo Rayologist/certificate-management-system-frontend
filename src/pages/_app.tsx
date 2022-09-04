aa5b2e0acb772ace96b9cfd576b05980d8421846
Bo-wei Chen <rayologist1002@gmail.com>
2022-09-04 18:06:42 +0800

    Add title and meta to `Head` tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,6 +34,11 @@ function App(props: AppPropsWithLayout & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
+        <title>NTU CBE - Certificate Management System</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
         <link
           rel="shortcut icon"
           href="/favicon.ico"
