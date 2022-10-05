57d4a93daad06b52e82b4cd0e0078d2fd85db846
Bo-wei Chen <rayologist1002@gmail.com>
2022-10-05 19:46:20 +0800

    Modify according to mantine-formik repository

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,5 +1,8 @@
 import FormikController from './FormikController';
 import useSimpleForm from './SimpleForm';
 import { useCustomFormik } from './FormikComponents/Helper';
+import ErrorMessage from './FormikComponents/Helper/ErrorMessage';
 
-export { FormikController, useCustomFormik, useSimpleForm };
+export { FormikController, useCustomFormik, ErrorMessage };
+
+export default useSimpleForm;
