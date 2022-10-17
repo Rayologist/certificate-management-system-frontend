6d8375131c15cfa1592939809dcf43c9474d3b06
Bo-wei Chen <rayologist1002@gmail.com>
2022-10-17 16:00:54 +0800

    Configure bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/certificate',
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
