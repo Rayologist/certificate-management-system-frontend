7268047518f6eb9858b32495375cfa4785cc2fc1
Bo-wei Chen <rayologist1002@gmail.com>
2022-10-17 15:43:12 +0800

    Update hook according to mantine-formik

--- src/components/Form/SimpleForm.tsx
+++ src/components/Form/SimpleForm.tsx
@@ -1,39 +1,50 @@
-import { Form, Formik, FormikConfig } from 'formik';
+import { Form, Formik, FormikConfig, FormikContextType } from 'formik';
+import { Button, Grid, ButtonProps, useMantineTheme } from '@mantine/core';
 import { FormikController } from '@components/Form';
-import { Button, Grid, ButtonProps } from '@mantine/core';
-import { ControllerPropsWithCol } from 'types';
+import { SimpleFormControllerProps } from 'types';
 import { useId } from '@mantine/hooks';
+import { ReactNode } from 'react';
 
 type Fields = {
   [field: string]: any;
 };
 
-type SimpleFormProps<T extends Fields> = FormikConfig<T> & ControllerPropsWithCol;
+type SimpleFormProps<T extends Fields> = FormikConfig<T> &
+  SimpleFormControllerProps<FormikContextType<T>>;
 
 const useSimpleForm = <T extends Fields>(props: SimpleFormProps<T>) => {
   const id = useId();
+  const theme = useMantineTheme();
 
-  const { controllers, ...rest } = props;
+  const { controllers, ...formikProps } = props;
 
-  const FormikWrapper = () => (
-    <Formik {...rest}>
-      <Form id={id}>
-        <Grid justify="center" gutter="xl">
-          {controllers.map((field, index) => {
-            const { col } = field;
-            return (
-              <Grid.Col key={`${field.name}-${index}`} {...col}>
-                <FormikController {...field} />
-              </Grid.Col>
-            );
-          })}
-        </Grid>
-      </Form>
+  const FormikWrapper = ({
+    children,
+  }: {
+    children?: ReactNode | ((formikContext: FormikContextType<T>) => ReactNode);
+  }) => (
+    <Formik {...formikProps}>
+      {(formik) => (
+        <Form id={id}>
+          <Grid justify="center" gutter="xl">
+            {controllers.map((field, index) => {
+              const { col, after, ...controllerProps } = field;
+              return (
+                <Grid.Col key={`${field.name}-${index}`} {...col}>
+                  <FormikController {...controllerProps} />
+                  {typeof after === 'function' ? after(formik) : after}
+                </Grid.Col>
+              );
+            })}
+          </Grid>
+          {typeof children === 'function' ? children(formik) : children}
+        </Form>
+      )}
     </Formik>
   );
 
   FormikWrapper.Button = (buttonProps: ButtonProps) => (
-    <Button type="submit" form={id} {...buttonProps}>
+    <Button type="submit" form={id} loaderProps={{ color: theme.colors.blue[5] }} {...buttonProps}>
       {buttonProps.children}
     </Button>
   );
