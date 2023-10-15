21092d58f19550bb28adbe9f9fac380f973f680f
Bowei Chen <bwchen.dev@gmail.com>
2023-10-15 20:17:58 +0800

    Fix floating error message

--- src/components/Form/FormikComponents/Helper/ErrorMessage.tsx
+++ src/components/Form/FormikComponents/Helper/ErrorMessage.tsx
@@ -1,15 +1,17 @@
-import { Group, Text, TextProps } from '@mantine/core';
+import { Group, Text, TextProps, Box } from '@mantine/core';
 import { IconAlertCircle } from '@tabler/icons-react';
 
 const ErrorMessage = (props: { text: string; textProps?: TextProps }) => {
   const { text, textProps } = props;
   return (
-    <Group spacing={5} sx={{ position: 'absolute' }}>
-      <IconAlertCircle width={18} />
-      <Text weight={500} size="sm" {...textProps}>
-        {text}
-      </Text>
-    </Group>
+    <Box sx={{ position: 'relative' }}>
+      <Group spacing={5} sx={{ position: 'absolute' }}>
+        <IconAlertCircle width={18} />
+        <Text weight={500} size="sm" {...textProps}>
+          {text}
+        </Text>
+      </Group>
+    </Box>
   );
 };
 
