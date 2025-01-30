752033ff8bd3f5316f5cc0da586740f691d3e290
Bowei Chen <bwchen.dev@gmail.com>
2025-01-31 02:53:21 +0800

    Update basePath configuration for production environment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/certificate',
+  basePath: process.env.NODE_ENV === 'production' ? '/certificate' : '',
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
